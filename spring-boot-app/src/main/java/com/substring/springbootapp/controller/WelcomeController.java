eb67f76f8b75710700b844b458af796b0c6933d7
sonalichaudhari200 <sonalichaudhari200@gmail.com>
2026-09-11 18:09:38 +0530

    initial project pushed

---
+++ spring-boot-app/src/main/java/com/substring/springbootapp/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.substring.springbootapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController {
+    @GetMapping()
+    public ResponseEntity<String> welcome(){
+        return ResponseEntity.ok("Welcome to springboot app");
+
+
+
+    }
+
+
+
+
+}
